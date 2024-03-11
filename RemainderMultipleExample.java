fa47079195e645f723c894e313caddac7f8b0ba2
JuanSBJJ <163040736+JuanSBJJ@users.noreply.github.com>
2024-03-11 14:05:08 -0500

    Update RemainderMultipleExample.java

--- RemainderMultipleExample.java
+++ RemainderMultipleExample.java
@@ -1,8 +1,7 @@
 package paquete
 public class Multiple{
 public static void main( String args[]){
-	int number = 12600;
-	System.out.println();
+	int number = 12600; //Insert number to verify
 	int remainder = numero%100;
 	System.out.println(residuo > 0 ? "not multiple of 100" : "multiple of 100");
 	}
